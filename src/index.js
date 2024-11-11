24c1a05ef120364ee76b3be3476a6fa2d5dd4a21
Carlos William <carloswilliamwilliam7almeida@gmail.com>
2024-11-11 15:04:29 -0300

    Classificador de heroi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Teste");
+nomeUsuario  = "Carlos";
+xpUsuario = 1500;
+nivelUsuario = "";
+
+if (xpUsuario < 1000){
+    nivelUsuario = "Ferro";
+}else if( xpUsuario > 1000 && xpUsuario <= 2000){
+    nivelUsuario = "Bronze";
+}else if(xpUsuario > 2000 && xpUsuario <= 5000){
+    nivelUsuario = "Prata";
+}else if(xpUsuario > 5000 && xpUsuario <= 7000){
+    nivelUsuario = "Ouro";
+}else if(xpUsuario > 7000 && xpUsuario <= 8000){
+    nivelUsuario = "Platina";
+}else if(xpUsuario > 8000 && xpUsuario <= 9000){
+    nivelUsuario = "Ascendente";
+}else if(xpUsuario > 9000 && xpUsuario <= 10000){
+    nivelUsuario = "Imortal";
+}else if(xpUsuario >= 10001){
+    nivelUsuario = "Radiante";
+}
+
+console.log(`O herói de nome ${nomeUsuario} está no nível ${nivelUsuario}`);
+
